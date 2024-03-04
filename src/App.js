198f1a0a199c256a0b64e426dc218d8c58753617
aneebamoezz <anibakaleem@gmail.com>
2024-03-04 13:56:37 +0500

    commit initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ScrollToSection from './components/ScrollToSection';
+
+function App() {
+  return (
+    <div className="App">
+      <ScrollToSection />
+    </div>
+  );
+}
+
+export default App;
